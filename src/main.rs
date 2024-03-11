8fa5b38397a8b3d9985a259dde99cff18bdec21f
yuiiio <atbjyk@protonmail.com>
2024-03-12 00:17:30 +0900

    todo

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-use nix::sys::ioctl;
-use nix::ioctl_write_buf;
+//use nix::sys::ioctl;
+use nix::fcntl::{open, OFlag};
+use nix::unistd::close;
+use nix::ioctl_readwrite;
 
 // from i8kutils
 // #define I8K_SET_FAN		_IOWR('i', 0x87, size_t)
 const I8K_FAN_MAGIC: u8 = b'i';
 const I8K_FAN_SET: u8 = 0x87;
+ioctl_readwrite!(i8k_fan_set, I8K_FAN_MAGIC, I8K_FAN_SET, u64);
 
 fn main() {
+    let i8k_fd = open(
+        "/proc/i8k",
+        OFlag::O_RDWR,
+        nix::sys::stat::Mode::empty(),
+        ).unwrap();
 
-    let data: [u32; 2] = [0, 1]; // right, fan_speed[0, 1, 2]
-    ioctl_write_buf!(i8k_fd, I8K_FAN_MAGIC, I8K_FAN_SET, &[u32; 2]);
+    unsafe {
+        let mut data: u64 = (1 as u64) << 32 | 0 as u64;
+        // upper u32: {fan_speed [0, 1, 2]}
+        // down u32: {0: right_fan, 1: left_fan}
+        let _ = i8k_fan_set(i8k_fd, &mut data);
+    }
+
+    let _ = close(i8k_fd);
     println!("Hello, world!");
 }
